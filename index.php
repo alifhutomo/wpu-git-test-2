242c4905867ebed98478fa31560c62b392b46f66
alifhutomo <alifhutomo@gmail.com>
2023-01-25 15:33:53 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quam consequuntur, similique mollitia dolor quibusdam vel minus odit veniam perspiciatis nihil quasi, ut, repellendus laudantium inventore. Neque sit necessitatibus esse.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
